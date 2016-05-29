b714c2b601ccc51bb6b2d833df4d02fe1ac721a9
Jerry Zhao <jerry@coach360.net>
2016-05-29 09:20:12 -0400

    Project created

---
+++ cosmetics-keeper-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cosmetics-keeper-ios
+//
+//  Created by Jerry on 5/29/16.
+//  Copyright © 2016 tinyideas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
